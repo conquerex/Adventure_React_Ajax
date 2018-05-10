f3953b943f5907018fca5f1d0ef2b17ca1149373
unknown <conquerex@gmail.com>
2018-05-10 22:52:07 +0900

    Add Header - component js, style css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Header } from './components'
 
 class App extends Component {
   render() {
     return (
       <div>
-        test
+        <Header/>
       </div>
     );
   }
