1a8a84addd416e61d509304003dc8224ba33eebf
손종국 <conquerex@gmail.com>
2018-05-11 17:52:44 +0900

    Call fetchPostInfo on componentDidMount

--- src/containers/PostContainer/PostContainers.js
+++ src/containers/PostContainer/PostContainers.js
@@ -4,11 +4,21 @@ import * as service from '../../services/post';
 
 class PostContainers extends Component {
 
+    componentDidMount() {
+        this.fetchPostInfo(1);
+    }
+    
     fetchPostInfo = async(postId) => {
-        const post = await service.getPost(postId);
-        console.log(post);
-        const comments = await service.getComments(postId);
-        console.log(comments)
+        // 효율적인 비동기 요청
+        const info = await Promise.all([
+            service.getPost(postId),
+            service.getComments(postId)
+        ]);
+        // const post = await service.getPost(postId);
+        // console.log(post);
+        // const comments = await service.getComments(postId);
+        // console.log(comments)
+        console.log(info);
     }
 
     render() {
